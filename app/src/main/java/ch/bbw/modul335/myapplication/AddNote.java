b332f7ba13717fc0fcd602a1cf6f55cea2c4065b
ayu-ahilan <ahilayu@hotmail.com>
2021-12-09 13:30:07 +0100

    Date Picker

--- app/src/main/java/ch/bbw/modul335/myapplication/AddNote.java
+++ app/src/main/java/ch/bbw/modul335/myapplication/AddNote.java
@@ -2,6 +2,8 @@ package ch.bbw.modul335.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -9,15 +11,21 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddNote extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     ImageView preview;
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
 
-    private void takePhoto() {
+    public void takePhoto(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
     }
@@ -27,9 +35,77 @@ public class AddNote extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+        initDatePicker();
+        dateButton = findViewById(R.id.datePickerButton);
+        dateButton.setText(getTodaysDate());
     }
 
-    /*@Override
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+    }
+
+    private String makeDateString(int day, int month, int year) {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month) {
+        if (month == 1)
+            return "JAN";
+        if (month == 2)
+            return "JAN";
+        if (month == 3)
+            return "JAN";
+        if (month == 4)
+            return "JAN";
+        if (month == 5)
+            return "JAN";
+        if (month == 6)
+            return "JAN";
+        if (month == 7)
+            return "JAN";
+        if (month == 8)
+            return "JAN";
+        if (month == 9)
+            return "JAN";
+        if (month == 10)
+            return "JAN";
+        if (month == 11)
+            return "JAN";
+        if (month == 12)
+            return "JAN";
+        // never happen
+            return "JAN";
+
+
+
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
@@ -38,5 +114,9 @@ public class AddNote extends AppCompatActivity {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             preview.setImageBitmap(imageBitmap);
         }
-    }*/
+    }
+
+    public void openDatePicker(View view) {
+        datePickerDialog.show();
+    }
 }
