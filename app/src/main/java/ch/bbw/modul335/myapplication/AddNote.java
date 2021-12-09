ca2ab56a19e95fe8034302af10b09937f1798a9c
ayu-ahilan <ahilayu@hotmail.com>
2021-12-09 11:33:22 +0100

    Camera funktionen

--- app/src/main/java/ch/bbw/modul335/myapplication/AddNote.java
+++ app/src/main/java/ch/bbw/modul335/myapplication/AddNote.java
@@ -2,14 +2,41 @@ package ch.bbw.modul335.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class AddNote extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    ImageView preview;
+
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
     }
+
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            preview = (ImageView) findViewById(R.id.preview);
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            preview.setImageBitmap(imageBitmap);
+        }
+    }*/
 }
